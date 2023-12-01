5f459075876ce1b89a6ca1f50489151f4e236978
Matej Mazúr <matejmazur@outlook.com>
2023-12-01 23:31:51 +0100

    Add some more design

--- Web/src/routes/+page.svelte
+++ Web/src/routes/+page.svelte
@@ -1,22 +1,19 @@
-<div class="w-screen h-screen absolute">
+<div class="w-screen h-screen absolute invisible md:visible" xmlns="http://www.w3.org/1999/html">
     <img src="Img/Waves/Vector.png" class="absolute bottom-0" alt="Wave1"/>
     <img src="Img/Waves/Vector-1.png" class="absolute bottom-0" alt="Wave2"/>
     <img src="Img/Waves/Vector-2.png" class="absolute bottom-0" alt="Wave3"/>
 </div>
 
 
-<div class="container mx-auto h-screen flex justify-center">
-    <div class="card bg-base-100 shadow mx-auto h-1/3">
-        <div class="card-body max-h-full">
-            <div class="aspect-square max-h-full">
-                <img src="MatejMa2ur.png" alt="Personal" class="max-h-full"/>
+<div class="container mx-auto h-screen flex flex-col md:flex-row gap-5
+            md:justify-around justify-evenly items-center">
+    <h1 class="text-6xl lg:text-9xl font-extrabold pl-5">
+        Impossible is <br>
+        <span class="text-primary">opinion</span>
+    </h1>
+    <div class="aspect-square h-1/3 md:h-1/2">
+        <img src="MatejMa2ur.png" alt="Personal" class="max-h-full"/>
 
-            </div>
-
-            <h1>
-                Hello
-            </h1>
-        </div>
     </div>
 </div>
 
